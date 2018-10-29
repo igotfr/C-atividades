ce881fd3025c73ece9a458668ce219533a69e575
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2018-10-29 20:36:34 -0300

    Update 1.9 divisores.c

--- Medium/1.9 divisores.c
+++ Medium/1.9 divisores.c
@@ -14,25 +14,39 @@ Ao final, imprima também a soma dos divisores encontrados.
 
 struct modeloDivisores
 {
-  int *divisores, qtdDivisores;
+  int *divisores, qtdDivisores, somaDivisores;
 };
 
 struct modeloDivisores divisores(int n)
 {
   struct modeloDivisores atributosDivisores;
+
   atributosDivisores.divisores= malloc(0);
   atributosDivisores.qtdDivisores= 0;
+  atributosDivisores.somaDivisores= 0;
 
   for(int i= 1; i<=n/2; i++)
     if(n%i == 0)
     {
       atributosDivisores.divisores[atributosDivisores.qtdDivisores]= i;
       atributosDivisores.qtdDivisores++;
+      atributosDivisores.somaDivisores+= i;
     }
 
   atributosDivisores.divisores[atributosDivisores.qtdDivisores]= n;
+  atributosDivisores.somaDivisores+= n;
 
   return atributosDivisores;
+}
+
+int somaDivisores(struct modeloDivisores atributosDivisores)
+{
+  int somaDivisores= 0;
+
+  for(int i= 0; i<=atributosDivisores.qtdDivisores; i++)
+    somaDivisores+= atributosDivisores.divisores[i];
+
+  return somaDivisores;
 }
 
 void exibirDivisores(struct modeloDivisores atributosDivisores)
@@ -44,11 +58,14 @@ void exibirDivisores(struct modeloDivisores atributosDivisores)
 int main()
 {
   int n;
+  struct modeloDivisores retornoDivisores;
 
   printf("Digite 1 número para ver seus divisores:");
   scanf("%d", &n);
+  retornoDivisores= divisores(n);
+  exibirDivisores(retornoDivisores);
 
-  exibirDivisores(divisores(n));
+  printf("\n\nSoma dos divisores: %i", retornoDivisores.somaDivisores);
 
   return 0;
 }
