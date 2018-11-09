a0708e828307ed7ef5be9b60b5df792023f9dcaa
cindRoberta <32280512+cindRoberta@users.noreply.github.com>
2018-11-09 18:28:21 -0200

    Update 2.2.1 contabilidade de mercadorias.c

--- Simplest Lista 2.2/2.2.1 contabilidade de mercadorias.c
+++ Simplest Lista 2.2/2.2.1 contabilidade de mercadorias.c
@@ -22,14 +22,14 @@ int mercadorias()
 		printf("Preço de venda: ");
 		scanf("%f", &precoVenda);
 
-    printf("\n");
+    		printf("\n");
 
 		// CÁLCULO
 		totalCompra+= precoCompra;
 		totalVenda+= precoVenda;
 
 		lucroTotal+= precoVenda - precoCompra;
-		lucroPercentual= 100*((precoVenda/precoCompra)-1);
+		lucroPercentual= 100*((precoVenda/precoCompra) -1);
 
 		if( lucroPercentual<10 )
 			qtdMercadoriascomMenos10porcentoLucro++;
@@ -40,18 +40,19 @@ int mercadorias()
 	}
 
 	// APRESENTAÇÃO DE RESULTADOS
-  printf("\n");
+	printf("\n");
 
 	printf("Quantidade de mercadorias com lucro menor que 10%%: %i\n", qtdMercadoriascomMenos10porcentoLucro);
 	printf("Quantidade de mercadorias com lucro entre 10%% e 20%%: %i\n", qtdMercadoriascomLucroentre10e20);
 	printf("Quantidade de mercadorias com lucro maior que 20%%: %i\n\n", qtdMercadoriascomMais20porcentoLucro);
-  printf("Valor total de compra de mercadorias: %.4f\n", totalCompra);
-  printf("Valor total de venda de mercadorias: %.4f\n\n", totalVenda);
-  printf("Lucro total: %.4f\n", lucroTotal);
+	printf("Valor total de compra de mercadorias: %.4f\n", totalCompra);
+	printf("Valor total de venda de mercadorias: %.4f\n\n", totalVenda);
+	printf("Lucro total: %.4f\n", lucroTotal);
 }
 
 int main()
 {
 	mercadorias();
+
 	return 0;
 }
